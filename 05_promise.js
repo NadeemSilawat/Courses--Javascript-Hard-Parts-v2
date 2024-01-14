430c1e15a0185e438bf41540264a51dbd7d7891b
NadeemSilawat <nadeemyunussilwat@gmail.com>
2024-01-14 12:31:11 +0530

    Added

--- 05_promise.js
+++ 05_promise.js
@@ -4,7 +4,24 @@ function display(data){
     console.log(data)
 }
 
-const futureData = fetch('https://in.linkedin.com/in/nadeem-silawat-560987274')
+const futureData = fetch('https://www.google.com')
 futureData.then(display);
-
+// console.log(futureData);
 console.log("Me first");
+
+// But we need to know how our promise-deferred functionality gets back into JavaScript to be run
+
+function dsply(data){console.log(data)}
+function printHello(){console.log("Hello");}
+
+function blockFor300ms(){/*block js thread for 300ms*/}
+
+
+setTimeout(printHello,0);
+
+const furtureData = fetch('https//www.google.com')
+
+furtureData.then(display)
+
+blockFor300ms()
+console.log("Me First");
