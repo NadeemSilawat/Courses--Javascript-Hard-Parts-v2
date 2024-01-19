3d729346ef4be4145aa54dd37668f5a915ca1e7d
NadeemSilawat <nadeemyunussilwat@gmail.com>
2024-01-19 20:04:41 +0530

    Changes and Added code

--- 06_classes_and_prototypes.js
+++ 06_classes_and_prototypes.js
@@ -8,15 +8,61 @@ const user1 = {
     increment : function(){user1.score++;}
 };
 
-user1,increment();
+user1.increment();
 
 
 // Creating user2 user dot notation
 
+// Declare an empty object and add properties with dot notation
+
+
+
 const user2 = {}; //create an empty object
+
+
 //assign properties to that object
 user2.name = "Tim";
 user2.score = 6;
 user2.increment = function() {
  user2.score++;
 };
+
+/*******************************************/
+
+//Factory Function .............
+
+
+// Creating user3 using Object.create  //
+
+const user3 = Object.create(null);
+
+user3.name = "Eve";
+user3.score = 9;
+user3.increment = function(){
+    user3.score++;
+
+}
+console.log(user3);
+console.log(user3.increment());
+console.log(user3);
+
+
+/*************************************************/
+
+// Solution 1. Generate objects using a function
+
+
+function userCreator(names,score){
+    const newUser = {};
+    newUser.names = names;
+    newUser.score= score;
+    newUser.increment = () => newUser.score++ ;
+
+    return newUser;
+}
+
+const person1 = userCreator("will",6);
+const person2 = userCreator("Tim",2);
+
+console.log(person1)
+
